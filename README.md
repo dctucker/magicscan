06c6c3c986ec5aa8f7d4a705ec4911a8d38e2918
D. Casey Tucker <dcaseytucker@gmail.com>
2017-08-27 08:31:06 -0700

    initial commit, naïve image OCR compared against JSON text

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # magicscan
 Scan and recognize Magic the Gathering cards
+
+
+## Setup
+You'll need Python 2.7, OpenCV, and Tesseract.
+On Mac OS it's helpful to use a virtual environment to avoid package collisions:
+
+virtualenv .
+source bin/activate
+
+Run pip install dependent packages:
+
+pip install -r requirements.txt
+
+## Running
+
+python ocr.py -i 140.jpg -a text
+
+
+## References
+
+1. https://github.com/mtgjson/mtgjson
+2. http://magiccards.info/query?q=wolf&v=card&s=cname
---
+++ ocr.py
@@ -0,0 +1,73 @@
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import os
+import json
+from difflib import SequenceMatcher
+from operator import itemgetter
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image to be OCR'd")
+ap.add_argument("-a", "--attr", type=str, default="name",
+	help="attribute to search on")
+ap.add_argument("-p", "--preprocess", type=str, default="thresh",
+	help="type of preprocessing to be done")
+args = vars(ap.parse_args())
+
+search_on_attr = args['attr']
+
+
+# load the example image and convert it to grayscale
+image = cv2.imread(args["image"])
+image = cv2.resize(image, None, fx = 2, fy = 2, interpolation = cv2.INTER_CUBIC)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ 
+# check to see if we should apply thresholding to preprocess the
+if args["preprocess"] == "thresh":
+	gray = cv2.threshold(gray, 0, 255,
+		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+elif args["preprocess"] == "blur":
+	gray = cv2.medianBlur(gray, 3)
+ 
+# write the grayscale image to disk as a temporary file so we can
+# apply OCR to it
+filename = "{}.png".format(os.getpid())
+cv2.imwrite(filename, gray)
+
+
+# load the image as a PIL/Pillow image, apply OCR, and then delete
+# the temporary file
+text = pytesseract.image_to_string(Image.open(filename))
+text = text.replace("\n", " ")
+text = text.replace("  ", " ")
+text = text.replace("- ", "-")
+os.remove(filename)
+print(text)
+
+
+with open('AllCards.json') as f:
+	cards = json.load(f)
+
+matches = []
+for name,card in cards.items():
+	if 'text' in card:
+		ratio = similar(text, card[search_on_attr])
+		matches.append( (ratio, card,) )
+
+matches = sorted( matches, key=itemgetter(0), reverse=True )
+for match in matches[0:5]:
+	print match[0], match[1]['name'],
+	print search_on_attr, '=', match[1][search_on_attr],
+	print
+
+ 
+# show the output images
+#cv2.imshow("Image", image)
+cv2.imshow("Output", gray)
+cv2.waitKey(0)
+
