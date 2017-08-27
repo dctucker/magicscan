53fb92f6a7d7fd7c0f5e1231d2084e3ec23cb008
D. Casey Tucker <dcaseytucker@gmail.com>
2017-08-27 09:09:46 -0700

    refactored to functional approach

--- src/ocr.py
+++ src/ocr.py
@@ -22,52 +22,59 @@ args = vars(ap.parse_args())
 search_on_attr = args['attr']
 
 
-# load the example image and convert it to grayscale
-image = cv2.imread(args["image"])
-image = cv2.resize(image, None, fx = 2, fy = 2, interpolation = cv2.INTER_CUBIC)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
- 
-# check to see if we should apply thresholding to preprocess the
-if args["preprocess"] == "thresh":
-	gray = cv2.threshold(gray, 0, 255,
-		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-elif args["preprocess"] == "blur":
-	gray = cv2.medianBlur(gray, 3)
- 
-# write the grayscale image to disk as a temporary file so we can
-# apply OCR to it
-filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, gray)
+def scan_image(filename):
+	# load the image, resize to 2x, convert to grayscale
+	image = cv2.imread(filename)
+	image = cv2.resize(image, None, fx = 2, fy = 2, interpolation = cv2.INTER_CUBIC)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	 
+	# apply threshold or blur
+	if args["preprocess"] == "thresh":
+		gray = cv2.threshold(gray, 0, 255,
+			cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+	elif args["preprocess"] == "blur":
+		gray = cv2.medianBlur(gray, 3)
+	 
+	# write grayscale image to disk as a temporary file for ORC
+	filename = "{}.png".format(os.getpid())
+	cv2.imwrite(filename, gray)
 
+	# debug: show images
+	#cv2.imshow("Image", image)
+	#cv2.imshow("Output", gray)
+	#cv2.waitKey(0)
 
-# load the image as a PIL/Pillow image, apply OCR, and then delete
-# the temporary file
-text = pytesseract.image_to_string(Image.open(filename))
-text = text.replace("\n", " ")
-text = text.replace("  ", " ")
-text = text.replace("- ", "-")
-os.remove(filename)
-print(text)
+	# load the image as a PIL/Pillow image
+	text = pytesseract.image_to_string(Image.open(filename))
+	os.remove(filename)
+	text = text.replace("\n", " ")
+	text = text.replace("  ", " ")
+	text = text.replace("- ", "-")
+	return text
 
+def scan_database(search_on_attr, text):
+	with open('data/AllCards.json') as f:
+		cards = json.load(f)
 
-with open('data/AllCards.json') as f:
-	cards = json.load(f)
+	matches = []
+	for name,card in cards.items():
+		if 'text' in card:
+			ratio = similar(text, card[search_on_attr])
+			matches.append( (ratio, card,) )
 
-matches = []
-for name,card in cards.items():
-	if 'text' in card:
-		ratio = similar(text, card[search_on_attr])
-		matches.append( (ratio, card,) )
+	matches = sorted( matches, key=itemgetter(0), reverse=True )
+	return matches
 
-matches = sorted( matches, key=itemgetter(0), reverse=True )
-for match in matches[0:5]:
-	print match[0], match[1]['name'],
-	print search_on_attr, '=', match[1][search_on_attr],
-	print
+def print_matches(matches):
+	for match in matches[0:5]:
+		print match[0], match[1]['name'],
+		print search_on_attr, '=', match[1][search_on_attr],
+		print
 
- 
-# show the output images
-#cv2.imshow("Image", image)
-cv2.imshow("Output", gray)
-cv2.waitKey(0)
-
+text = scan_image(args["image"])
+print "Scanned text:"
+print text
+matches = scan_database(args['attr'], text)
+print
+print "Matches:"
+print_matches(matches)
