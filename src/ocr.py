965a8a2a665d26c56fc9b02e068fac397a7cc1e6
Areli Jimenez Huerta <areli.jimenez.huerta@gmail.com>
2017-08-28 21:30:48 -0700

    tweaking segmentation parameters

--- src/ocr.py
+++ src/ocr.py
@@ -48,30 +48,36 @@ def scan_image(filename):
 
 def segment_and_scan(filename):
 	image = cv2.imread(filename)
-	height, width, channels = image.shape
+	image = cv2.resize(image, None, fx = 2, fy = 2, interpolation = cv2.INTER_CUBIC)
+	h, w, channels = image.shape
 	filename = "{}.png".format(os.getpid())
 
 	#contours,hierarchy = cv2.findContours(0,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 	#cnt = contours[0]
 	#x,y,w,h = cv2.boundingRect(cnt)
 
+	#lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+	#l, a, b = cv2.split(lab)
+	#clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+	#cl = clahe.apply(l)
+	#gray = cl
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	#gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-	title_image = gray[ 0:int(height * 0.10), 1:int(width * 0.85) ] 
+	title_image = gray[ 0:int(0.10 * h), int(0.04 * w):int(0.85 * w) ] 
 	cv2.imwrite(filename, title_image)
 	text = pytesseract.image_to_string(Image.open(filename))
 	title = text.split("\n")[0]
 	os.remove(filename)
 
-	description_image = gray[ int(height * 0.55):int(height * 0.93), int(width * 0.05):int(width * 0.95) ] 
+	description_image = gray[ int(0.63 * h):int(0.93 * h), int(0.05 * w):int(0.95 * w) ]
 	cv2.imwrite(filename, description_image)
 	text = pytesseract.image_to_string(Image.open(filename))
 	description = text
 	os.remove(filename)
 
-	#cv2.imshow("Title", title_image)
-	#cv2.imshow("Description", description_image)
+	cv2.imshow("Title", title_image)
+	cv2.imshow("Description", description_image)
 	#cv2.waitKey(0)
 
 	return title, description
@@ -109,8 +115,8 @@ def print_matches(matches):
 
 #text = scan_image(args["image"])
 title, description = segment_and_scan(args["image"])
-#print "Scanned text:"
-#print title, description
+print "Scanned text:"
+print title, '/', description
 #print
 matches = scan_database([('name', title, 1.0), ('text', description, 0.25)])
 print "Matches:"
