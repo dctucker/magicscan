4c469943b3f772630f546830dfbdc5e2d6723d54
D. Casey Tucker <dcaseytucker@gmail.com>
2017-08-28 20:14:31 -0700

    segmentation initial commit

--- src/ocr.py
+++ src/ocr.py
@@ -46,6 +46,25 @@ def scan_image(filename):
 	text = text.replace("- ", "-")
 	return text
 
+def segment_and_scan(filename):
+	image = cv2.imread(filename)
+	height, width, channels = image.shape
+
+	contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+	cnt = contours[0]
+	x,y,w,h = cv2.boundingRect(cnt)
+
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = gray[ 0:int(height * 0.10), 1:int(width * 0.85) ] 
+	gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+	filename = "{}.png".format(os.getpid())
+	cv2.imwrite(filename, gray)
+	text = pytesseract.image_to_string(Image.open(filename))
+	cv2.imshow("Output", gray)
+	cv2.waitKey(0)
+	return text
+
+
 def similarity(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
@@ -70,10 +89,11 @@ def print_matches(matches):
 		print args['attr'], '=', match[1][args['attr']]
 		print
 
-text = scan_image(args["image"])
+#text = scan_image(args["image"])
+text = segment_and_scan(args["image"])
 print "Scanned text:"
 print text
-matches = scan_database(args['attr'], text)
-print
-print "Matches:"
-print_matches(matches)
+#matches = scan_database(args['attr'], text)
+#print
+#print "Matches:"
+#print_matches(matches)
